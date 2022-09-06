27d51be7ed5329da26d58982c64d90ff4f2256ed
juhani-dev <juhani.kontulainen@gmail.com>
2022-09-06 13:40:00 +0300

    1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,9 +8,10 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part1={props.part1} exercises1={props.exercises1} />
+      <Part part2={props.part2} exercises2={props.exercises2} />
+      <Part part3={props.part3} exercises3={props.exercises3} />
+      
     </div>
   )
 }
@@ -18,6 +19,15 @@ const Total = (props) => {
   return(
     <div>
       <p> number of exercises {props.exercises1+props.exercises2+props.exercises3}</p>
+    </div>
+  )
+}
+const Part = (props) => {
+  return(
+    <div>
+      {props.part1} {props.exercises1}
+      {props.part2} {props.exercises2}
+      {props.part3} {props.exercises3}
     </div>
   )
 }
