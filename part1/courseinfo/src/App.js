931061da386629caf0b949d028d1d5a31554fb8c
juhani-dev <juhani.kontulainen@gmail.com>
2022-09-07 13:11:07 +0300

    1.7

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,22 +5,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
 
   return (
     <div>
       <h1>Give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)} >
+      <button onClick={() => { setGood(good + 1) ; setAll(all +1)}} >
         good
       </button>
-      <button onClick={() => setNeutral(neutral + 1)} >
+      <button onClick={() =>{ setNeutral(neutral + 1) ; setAll(all +1)}} >
         neutral
       </button>
-      <button onClick={() => setBad(bad + 1)} >
+      <button onClick={() => { setBad(bad + 1); setAll(all +1)}}>
         bad
       </button>
 
-      <h1>results</h1>
+      <h1>statistics</h1>
 
        
       good {good}
@@ -28,7 +30,12 @@ const App = () => {
       neutral {neutral}
       <br></br>
       bad {bad}
-       
+      <br></br>
+      all {all}
+      <br></br>
+      average {(good-bad)/all}
+      <br></br>
+      positive {good/all} %
       
        
     </div>
