7288471ffd13ebcbcce519936aad48d016a8adcc
juhani-dev <juhani.kontulainen@gmail.com>
2022-09-07 12:20:04 +0300

    1.6

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,66 +1,36 @@
-const Header = (props) => {
-  
-  return (
-    <div>
-      
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-const Content = (props) => {
-  console.log(props,"wew")
-  return (
-    <div>
-      <Part part={props.parts[0]}   />
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
-    </div>
-  )
-}
-const Total = (props) => {
-  
-  return(
-    <div>
-      <p> number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
-    </div>
-  )
-}
-const Part = (props) => {
-  console.log(props,"ss")
-  return(
-    <div>
-      {props.part.name} {props.part.exercises}
-       
-    </div>
-  )
-}
-const App = () => {
+import { useState } from 'react'
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts ={course.parts}/>
+      <h1>Give feedback</h1>
+
+      <button onClick={() => setGood(good + 1)} >
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)} >
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)} >
+        bad
+      </button>
+
+      <h1>results</h1>
+
+       
+      good {good}
+      <br></br>
+      neutral {neutral}
+      <br></br>
+      bad {bad}
+       
       
-      
+       
     </div>
   )
 }
