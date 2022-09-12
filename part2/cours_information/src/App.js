839cc1444f352c0d1fdb22f07722e4e505d14ab2
juhani-dev <juhani.kontulainen@gmail.com>
2022-09-12 12:47:31 +0300

    cours_information_2.5

--- part2/cours_information/src/App.js
+++ part2/cours_information/src/App.js
@@ -1,51 +1,6 @@
 
     
-const Course = ({course}) => {
-  return (
-    <>
-    <Header course={course}/>
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-    </>
-  )
-}
-
-const Header = ({course}) => {
-  return(
-    <h1>{course.name}</h1>
-  )
-}
-const Part = ({part}) => 
-  
-    <p>
-    {part.name} {part.exercises}
-    </p>
-   
-const Content = ({parts}) => {
-
-
-  return(
-  <>
-   {parts.map(line =>
-    <  Part key={line.id} part={line} />)}
-  </>
-  )
-}
-
-const Total = ({parts}) => {
-  
-  const total = 0
-  
-  const sum = parts.reduce(function (accumulator, curValue) {
-    console.log(curValue.exercises)
-    return accumulator + curValue.exercises
-  }, total)
-  
-  console.log(sum)
-return (
-  <>total of {sum} exercises</>
-)
-}
+import Course from './components/Course'
 
 const App = () => {
   const course =  [
