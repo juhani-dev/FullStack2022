4cfd8c5b8da714e3e2e57a401dfc49c0cd92c3da
juhani-dev <juhani.kontulainen@gmail.com>
2022-09-14 14:50:16 +0300

    2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,70 @@
 import { useState } from 'react'
+const AddPersonForm =({newNumber,handleNumberChange,handleNameChange,newName,setNewName,setPersons,persons})=>{
+  
+  return(
+    <form onSubmit={(event)=>AddPerson(event,persons,newName, newNumber,setPersons,setNewName)} >
+      <div>
+        number :<input value={newNumber} 
+        onChange={handleNumberChange}
+        />
+        <br></br>
+        name :<input value={newName} 
+        onChange={handleNameChange}
+        />
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+const AddPerson = (event,persons,newName, newNumber,setPersons,setNewName) => {
+  
+  event.preventDefault()
+  let check = false
+  persons.forEach((element) =>{      
+    if (JSON.stringify(element.name)===JSON.stringify(newName))   {
+     check =true
+  }
+  })
+  if (!check){
+    const person ={
+      name: newName,
+      number: newNumber,
+      id:persons.length+1
+      }
+        setPersons(persons.concat(person))
 
+    }
+    else {alert(JSON.stringify(newName+" is already added to the phonebook"))}
+      setNewName('')
+
+}
+
+
+const PersonsShow =({personToShow})=>{
+  return (
+    <ul>
+        {personToShow.map(note => <li key ={note.name}>
+          {note.name} {note.number} </li>)}
+      </ul>
+  )
+}
+const FilterList =({search,onChange})=>{
+  return(
+      <div>
+
+      filter numbers: 
+      <input
+      value={search}
+      onChange={onChange}/>
+      </div>
+  )
+}
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',number:'ddd',id:1 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -26,61 +88,21 @@ const App = () => {
   }
   const handleNumberChange = (event) => {
     console.log(event.target.value)
-    
     setNewNumber(event.target.value)
   }
-  const addPerson = (event) => {
-    event.preventDefault()
-    let check = false
-    persons.forEach((element) =>{      
 
-      if (JSON.stringify(element.name)===JSON.stringify(newName))   {
-  
-       check =true
-    }
-      
-  })
-  if (!check){
-  const person ={
-    name: newName,
-    number: newNumber,
-    id:persons.length+1
-  }
-setPersons(persons.concat(person))
-  
-
-}
-else {alert(JSON.stringify(newName+" is already added to the phonebook"))}
-setNewName('')
-  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        
-        filter list: <input value={search} onChange={handleSearchChange}/>
-      </div>
+      <FilterList
+          filter={search} onChange={handleSearchChange}/>
+      
       <h2>add new</h2>
-      <form  onSubmit={addPerson}>
-        <div>
-        number :<input value={newNumber} onChange={handleNumberChange}
-        />
-        <br></br>
-        name :<input value={newName} 
-
-        onChange={handleNameChange}
-        />
-          <button type="submit">add</button>
-        </div>
-      </form>
+     < AddPersonForm  newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}persons={persons} setPersons={setPersons}setNewName={setNewName}/>
+      
       <h2>Numbers</h2>
-     
-        <ul>
-        {personToShow.map(note => <li key ={note.name}>
-          {note.name} {note.number} </li>)}
-      </ul>
-      
-      
+     <PersonsShow personToShow={personToShow} />
+              
     </div>
     
   )
