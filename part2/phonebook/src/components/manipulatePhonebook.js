bf540e7977bb2645dee8bb9709cfd753c964a69e
juhani-dev <juhani.kontulainen@gmail.com>
2022-09-30 23:44:03 +0300

    tuning

--- part2/phonebook/src/components/manipulatePhonebook.js
+++ part2/phonebook/src/components/manipulatePhonebook.js
@@ -3,12 +3,14 @@ import personService from '../services/persons'
 
 export const AddPersonNew=(event,persons,newName,newNumber,setPersons,setNewName,setMessage,setErrorMessage)=>{
     event.preventDefault()
-  
+    
+    
     if ( CheckName(persons,newName) ){
       let id =FindId(persons,newName)
       HandleNumberUpdate(id,newName,newNumber,setPersons,persons,setMessage,setErrorMessage)
       
     }else if (CheckNumber(persons,newNumber)){
+      
       alert("this number is already in the phonebook")
       
     }else{
@@ -29,6 +31,7 @@ export const HandleNewPerson =(newName,newNumber,setNewName,setPersons,persons,s
        setNewName('')
       }) 
       setMessage(
+        
         `${newName} was added to the phonebook`
       )
       setTimeout(() => {
@@ -41,6 +44,7 @@ export const HandleNewPerson =(newName,newNumber,setNewName,setPersons,persons,s
 export const HandleNumberUpdate=(id,name,number,setPersons,persons,setMessage,setErrorMessage)=>{
     
     if (window.confirm("update "+name+" number?")) {
+      
     personService
     .update(id,name,number)
     .then(responseData =>{
@@ -53,21 +57,25 @@ export const HandleNumberUpdate=(id,name,number,setPersons,persons,setMessage,se
       }, 5000)
     })
     .catch(error => {
-        setErrorMessage("This person is already deleted!"
+      setErrorMessage("this person is already deleted")
+      setTimeout(()=>{
+          setErrorMessage(null)
+      },5000
       )
-        setTimeout(()=> {
-            setErrorMessage(null)
-        },5000
-        )
-        setPersons(persons.filter(person =>person.id != id))
-        
+  
+     
     })
+        
+        
+        
+    
     }
   
   }
 export const HandleDelete=(e,name,{persons,setPersons,setErrorMessage,setMessage})=>{
-    
+  
     if (window.confirm("Delete "+name+"?")){
+      console.log(e);
       personService
       .remove(e)
       .then(() =>{
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = '/api/persons'
+const baseUrl = "http://localhost:3001/api/persons"
 
 const getAll = () => {
     const request= axios.get(baseUrl)
@@ -14,12 +14,15 @@ const getAll = () => {
 
   const remove = id => {
     const request= axios.delete(baseUrl+"/"+id)
+    
     return request.then(response=>response.data)
   }
   const update = (id,name,number)=> {
+    
     const request =axios.put(baseUrl+"/"+id,{
         name,number
     })
+    
     return request.then(response=>response.data)
   }
     export default {getAll,create,remove,update}
