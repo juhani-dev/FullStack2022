c3ac596ca543fb0297d448c0a26e3ed7b3e9c68d
juhani-dev <juhani.kontulainen@gmail.com>
2022-10-07 14:52:39 +0300

    4.1-4.2

---
+++ part4/blogList/app.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter =require('./controllers/blogs')
+
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs',blogsRouter)
+
+
+
+module.exports = app
